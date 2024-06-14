ec56f09c49b3f6220bca0adec4aa3818b56a56ca
Taeyoung Won <hexter1994@gmail.com>
2024-06-14 15:18:17 +0900

    feat: 날씨 응답 모델 추가, 엔티티 변환 로직 구현

---
+++ WeatherSearch/Shared/Constant/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  WeatherSearch
+//
+//  Created by 원태영 on 6/14/24.
+//
+
+enum Constant {
+  static let celsiusBuffer: Double = 273.15
+}
---
+++ WeatherSearch/Shared/Extension/Double+.swift
@@ -0,0 +1,16 @@
+//
+//  Double+.swift
+//  WeatherSearch
+//
+//  Created by 원태영 on 6/14/24.
+//
+
+extension Double {
+  var asInt: Int {
+    return Int(self)
+  }
+  
+  var toCelsius: Double {
+    return self - Constant.celsiusBuffer
+  }
+}
